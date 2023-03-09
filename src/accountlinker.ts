96ab431146da33b1c5b9e77f5f3be13a1ac775c3
pwblais <pwblais@gmail.com>
2023-03-08 22:48:24 -0500

    New AccountLinker contract + Unlink handler

--- src/accountlinker.ts
+++ src/accountlinker.ts
@@ -1,6 +1,10 @@
 import { Account, User } from "../generated/schema";
-import { AccountLinked as AccountLinkedEvent } from "../generated/AccountLinker/AccountLinker";
+import {
+	AccountLinked as AccountLinkedEvent,
+	AccountUnlinked as AccountUnlinkedEvent,
+} from "../generated/AccountLinker/AccountLinker";
 import { createUser, increaseAccountCount } from "./helpers";
+import { store } from "@graphprotocol/graph-ts";
 
 export function handleAccountLinked(event: AccountLinkedEvent): void {
 	// Get the user entity
@@ -51,4 +55,34 @@ export function handleAccountLinked(event: AccountLinkedEvent): void {
 	// Save the user and account
 	account.save();
 	user.save();
+}
+
+export function handleAccountUnlinked(event: AccountUnlinkedEvent): void {
+	// Get the user entity
+	let user = User.load(event.params.player.toHexString());
+	// If the user doesn't exist, create it
+	if (user == null) {
+		user = createUser(
+			event.params.player,
+			event.block.number,
+			event.block.timestamp
+		);
+	}
+
+	// Check if the user already has an account for this platform.
+	// If so, update the userId.
+	for (let i = 0; i < user.accounts.length; i++) {
+		let account = Account.load(user.accounts[i]);
+		if (account) {
+			if (
+				account.platform == event.params.platform &&
+				account.uuid == event.params.uuid
+			) {
+				store.remove("Account", account.id);
+				user.accounts.splice(i, 1);
+				user.save();
+				return;
+			}
+		}
+	}
 }
